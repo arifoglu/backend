1b698f9b1502539362b17f26b7f8c13c25b6ba2f
arifoglu <arifoglu2020@gmail.com>
2022-04-19 17:52:06 +0200

    new update

--- studies/app.js
+++ studies/app.js
@@ -1,63 +1,44 @@
 const express = require('express');
-const morgan    = require('morgan');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
-// Create an express app
+// express app
 const app = express();
 
-//listen on port 3000
-app.listen(3000, () => {
-    console.log('express working on port 3000');
-    });
+// connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://arifoglu:12345@cluster0.aftnu.mongodb.net/node-lessons?retryWrites=true&w=majority'
+;
 
-//register the view engine
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
 app.set('view engine', 'ejs');
 
-
-//middleware 
+// middleware & static files
 app.use(express.static('public'));
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('in the next middleware');
-    next();
-  });
-  app.use(morgan('dev'));
-
-  app.use((req, res, next) => {
-    res.locals.path = req.path;
-    next();
-  });
-
-
-app.get('/', (req, res) => {
-    //res.send('<h1>Home page</h1>'); send html
-    const blogs = [
-        {title: 'Backend studies', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Backend comments', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Backend recommends', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', { title: 'Home', blogs });
+  res.locals.path = req.path;
+  next();
 });
 
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
 
 app.get('/about', (req, res) => {
-    //res.send('<h1>about page</h1>');
-    res.render('about',{ title: 'About' });
+  res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create',{ title: 'Create A new Blog' });
-});
+// blog routes
+app.use('/blogs', blogRoutes);
 
-//404 page
-app.use((req, res, next) => {
-    res.status(404).render('404',{ title: '404' });
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
